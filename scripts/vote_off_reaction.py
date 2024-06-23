0cce3aa601ec654b3281eee95a68f59436661e51
Matt Stiles <mattstiles@gmail.com>
2024-06-23 14:29:33 -0700

    adding a bunch of seasons

--- scripts/vote_off_reaction.py
+++ scripts/vote_off_reaction.py
@@ -1,5 +1,23 @@
 import os
+import json
 import pandas as pd
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Load the client secrets file
+service_account_file = "~/.google_service_account.json"
+
+# Define the scope
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive",
+]
+
+# Authenticate using the service account file
+credentials = Credentials.from_service_account_file(service_account_file, scopes=scope)
+
+# Authorize the gspread client
+client = gspread.authorize(credentials)
 
 """
 FETCH & SCORE
@@ -7,26 +25,62 @@ How did contestants respond after their torch was snuffed?
 This script fetches the growing log and scores the acknowlegement of his/her tribe.
 """
 
-# Define URL for Google Sheets CSV
-url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQfmJJvr0TdW6fZ-wDdTadFDY5wTm2S6cnr9-n2D3cS6-eRGbIySzVokY97bON66_BXDIc0jnSjch82/pub?gid=120496879&single=true&output=csv'
+# Output paths
+csv_output_path = "../data/raw/other/vote_offs/log.csv"
+json_output_path = "../data/raw/other/vote_offs/log.json"
 
-# Read CSV from URL
-df = pd.read_csv(url)
+# Open the Google Spreadsheet by name
+spreadsheet_name = "survivor_vote_offs"
+spreadsheet = client.open(spreadsheet_name).get_worksheet(0)
+
+# Fetch data from the worksheet
+data = spreadsheet.get_all_records()
+
+data_entry = pd.DataFrame(data)
+
+# clean castaways table
+vote_offs_lookup = pd.read_csv("../data/processed/survivor_all_voteoffs.csv")
+
+# Assertion to ensure the lengths are the same
+assert len(data_entry) == len(
+    vote_offs_lookup
+), f"Assertion failed: Lengths do not match (data_entry: {len(data_entry)}, vote_offs_lookup: {len(vote_offs_lookup)}"
+
+print("Lengths match successfully!")
+
+# Merge them together to add ids to Google Sheet
+merged = pd.merge(
+    data_entry, vote_offs_lookup, on=["season", "vote"], how="right", indicator=True
+)
+merged["match"] = merged["castaway"] == merged["voted_out"]
+
+assert merged[
+    "match"
+].all(), "Assertion failed: There are mismatched rows in the DataFrame."
+
+print("All rows matched successfully!")
+
+# List of columns to convert
+bool_columns = ["acknowledge", "ack_gesture", "ack_speak", "ack_look", "ack_smile"]
+
+# Convert the columns from string to boolean
+for col in bool_columns:
+    merged[col] = merged[col].map({"TRUE": True, "FALSE": False})
 
 # Define acknowledgment columns
-ack_columns = ['ack_gesture', 'ack_speak', 'ack_look', 'ack_smile']
+ack_columns = ["ack_gesture", "ack_speak", "ack_look", "ack_smile"]
 
 # Calculate acknowledgment score as the count of True values in the acknowledgment columns
-df['ack_score'] = df[ack_columns].sum(axis=1)
+merged["ack_score"] = merged[ack_columns].sum(axis=1)
 
 # Output paths
 csv_output_path = 'data/raw/other/vote_offs/log.csv'
 json_output_path = 'data/raw/other/vote_offs/log.json'
 
 # Save to CSV
-df.to_csv(csv_output_path, index=False)
+merged.to_csv(csv_output_path, index=False)
 
 # Save to JSON
-df.to_json(json_output_path, orient='records', lines=False, indent=4)
+merged.to_json(json_output_path, orient='records', lines=False, indent=4)
 
 print(f"Data saved to {csv_output_path} and {json_output_path}")
