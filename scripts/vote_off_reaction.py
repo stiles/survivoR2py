216230c20feed831d114fe061ab5e7ac6fd3f024
Matt Stiles <mattstiles@gmail.com>
2024-06-26 21:06:45 -0700

    adding season 12, cleaning up vote off log structure

--- scripts/vote_off_reaction.py
+++ scripts/vote_off_reaction.py
@@ -5,7 +5,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 # Load the client secrets file
-service_account_file = "~/.google_service_account.json"
+service_account_file = os.path.expanduser("~/.google_service_account.json")
 
 # Define the scope
 scope = [
@@ -22,12 +22,14 @@ client = gspread.authorize(credentials)
 """
 FETCH & SCORE
 How did contestants respond after their torch was snuffed? 
-This script fetches the growing log and scores the acknowlegement of his/her tribe.
+This script fetches the growing log and scores the acknowledgement of his/her tribe.
 """
 
-# Output paths
-csv_output_path = "../data/raw/other/vote_offs/log.csv"
-json_output_path = "../data/raw/other/vote_offs/log.json"
+# Determine the absolute paths for input and output files
+base_dir = os.path.dirname(os.path.abspath(__file__))
+csv_output_path = os.path.join(base_dir, "../data/processed/vote_offs/vote_off_log.csv")
+json_output_path = os.path.join(base_dir, "../data/processed/vote_offs/vote_off_log.json")
+vote_offs_lookup_path = os.path.join(base_dir, "../data/processed/survivor_all_voteoffs.csv")
 
 # Open the Google Spreadsheet by name
 spreadsheet_name = "survivor_vote_offs"
@@ -38,8 +40,8 @@ data = spreadsheet.get_all_records()
 
 data_entry = pd.DataFrame(data)
 
-# clean castaways table
-vote_offs_lookup = pd.read_csv("../data/processed/survivor_all_voteoffs.csv")
+# Clean castaways table
+vote_offs_lookup = pd.read_csv(vote_offs_lookup_path)
 
 # Assertion to ensure the lengths are the same
 assert len(data_entry) == len(
@@ -73,14 +75,15 @@ ack_columns = ["ack_gesture", "ack_speak", "ack_look", "ack_smile"]
 # Calculate acknowledgment score as the count of True values in the acknowledgment columns
 merged["ack_score"] = merged[ack_columns].sum(axis=1)
 
-# Output paths
-csv_output_path = 'data/raw/other/vote_offs/log.csv'
-json_output_path = 'data/raw/other/vote_offs/log.json'
+clean_cols = ['season', 'vote', 'episode', 'castaway', 'voted_out_id', 'acknowledge', 'ack_gesture', 'ack_speak',
+       'ack_look', 'ack_smile', 'ack_speak_notes', 'ack_score', 'notes']
+
+merged_clean = merged[clean_cols].rename(columns={'voted_out_id': 'castaway_id'}).copy()
 
 # Save to CSV
-merged.to_csv(csv_output_path, index=False)
+merged_clean.to_csv(csv_output_path, index=False)
 
 # Save to JSON
-merged.to_json(json_output_path, orient='records', lines=False, indent=4)
+merged_clean.to_json(json_output_path, orient='records', lines=False, indent=4)
 
 print(f"Data saved to {csv_output_path} and {json_output_path}")
